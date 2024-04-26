51f2326d2ea0b559d9853852094ac875e46913c6
zcpefaisal <inboxlions@gmail.com>
2024-04-27 03:29:47 +0600

    create a counter project (incress/decress the value) and understand about hooks and state

---
+++ l3-counter/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // const [counter, setCounter] = useState(0) // set is a convension
+  const [counter, faisalCounter] = useState(10) // 10 is the default value of counter [counter is variable, faisalCounter is function of counter set]
+  
+  // let counter  = 10 // programatically increass/decress the variable value, but in UI the update dose not impact/show
+
+  const increassValue = () => {
+    // counter = counter + 1
+    // console.log('incressed', counter)
+    // console.log(counter)
+
+    // counter should not incress getter then 20 
+    if (counter < 20){
+      faisalCounter(counter + 1) // actually state and UI syncing
+    }
+  }
+
+
+  const decressValue = () => {
+    // counter = counter - 1
+    // faisalCounter(counter - 1) // actually state and UI syncing
+    // console.log(counter)
+
+    // counter should not decress less then 0 
+    if (counter > 0){
+      faisalCounter(counter - 1)
+    }
+  }
+
+  return (
+    <>
+
+    {/* UI updata control form React and that backend works is hooks  */}
+
+      <h1>Our Counter </h1>
+      <h2>Counter Value: {counter}</h2>    
+
+      <button
+      onClick={increassValue}
+      >Incress</button>
+
+      <button
+      onClick={decressValue}
+      >Decress</button>
+
+    </>
+  )
+}
+
+export default App
