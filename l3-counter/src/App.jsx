1ffcd743add86c4a9b372d9bc091f35330b241ba
zcpefaisal <inboxlions@gmail.com>
2024-04-29 23:23:02 +0600

    state manage with previous state update of useState module

--- l3-counter/src/App.jsx
+++ l3-counter/src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 
 function App() {
 
-  // const [counter, setCounter] = useState(0) // set is a convension
-  const [counter, faisalCounter] = useState(10) // 10 is the default value of counter [counter is variable, faisalCounter is function of counter set]
+  // const [counter, faisalCounter] = useState(0) // set is a convension
+  const [counter, setCounter] = useState(10) // 10 is the default value of counter [counter is variable, faisalCounter is function of counter set]
   
   // let counter  = 10 // programatically increass/decress the variable value, but in UI the update dose not impact/show
 
@@ -14,9 +14,23 @@ function App() {
     // console.log(counter)
 
     // counter should not incress getter then 20 
-    if (counter < 20){
-      faisalCounter(counter + 1) // actually state and UI syncing
-    }
+    // if (counter < 20){
+    //   faisalCounter(counter + 1) // actually state and UI syncing
+    // }
+
+    // this 3 line execute same state for useState module, so that this type of multiple increment not work defaultly  
+    // setCounter(counter + 1) //same result
+    // setCounter(counter + 1) //same result
+    // setCounter(counter + 1) //same result
+
+
+    // use call back of call back for update previous state, and 4 line result will difference
+    // one line execution so that return not need
+    setCounter(prevCounter => prevCounter + 1)  // result
+    setCounter(prevCounter => prevCounter + 1)  // result + 1
+    setCounter(prevCounter => prevCounter + 1)  // result + 1
+    setCounter(prevCounter => prevCounter + 1)  // result + 1
+
   }
 
 
@@ -26,9 +40,15 @@ function App() {
     // console.log(counter)
 
     // counter should not decress less then 0 
-    if (counter > 0){
-      faisalCounter(counter - 1)
-    }
+    // if (counter > 0){
+    //   faisalCounter(counter - 1)
+    // }
+
+    // { } multi line execution so that return need
+    setCounter(prevCounter => { return prevCounter - 1})
+    setCounter(prevCounter => { return prevCounter - 1})
+    setCounter(prevCounter => { return prevCounter - 1})
+    setCounter(prevCounter => { return prevCounter - 1})
   }
 
   return (
@@ -39,13 +59,9 @@ function App() {
       <h1>Our Counter </h1>
       <h2>Counter Value: {counter}</h2>    
 
-      <button
-      onClick={increassValue}
-      >Incress</button>
+      <button onClick={increassValue} >Incress</button>
 
-      <button
-      onClick={decressValue}
-      >Decress</button>
+      <button onClick={decressValue} >Decress</button>
 
     </>
   )
